979e991f6b8cd4c43585e7cf9ce8f38003a90d03
Dorothy <dorahnakslove@gmail.com>
2024-12-12 11:13:05 +0300

    python exam solutions

--- question_four.py
+++ question_four.py
@@ -1,11 +1,30 @@
 '''
 # No.4 (i)(a)
+
+Object Oriented Programming invoves the used of classes and objects.
+
+- OOP helps programmers classfy different attributes
+- OOP aids software developers who deal with multiple things that are related to one another to make programs that unify them
+
+
 # No.4 (i)(b)
+
+A class refers to a container of different methods and objects
+An object is part of a class. You can have multiple objects of the safe class.
+
 '''
 
 # No.4 (ii)
 
-# sentence = input("Enter sentence: ")
+sentence = input("Enter sentence: ")
+occurences = sentence.split()
+print(occurences)
+
+
+occurence_dict = {}
+
+for word in sentence:
+    pass
 
 
 # No.4 (iii)
